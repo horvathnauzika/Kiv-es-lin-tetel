3447c7b2139e76596005fbc1fb377ffe31a37a94
horvathnauzika <naihorvath@gmail.com>
2024-01-31 15:22:35 +0100

    folytatas

---
+++ megszamolas.py
@@ -0,0 +1,8 @@
+# def megszamolas():
+#     db = 0
+#     for i in range(10, 99, 2):
+#         if i % 2 == 0:
+#             db += 1
+#     return db
+#
+# print(megszamolas())
---
+++ szelsoertek.py
@@ -0,0 +1,13 @@
+# def legkisebb():
+#     db = 0
+#     vege = 0
+#     min = 2147483648
+#     while ((szam := int(input("N= "))) != vege):
+#         if szam < min:
+#             min = szam
+#         db += 1
+#     return f"{db} számból a legkisebb: {min}"
+#
+#
+# print(legkisebb())
+
